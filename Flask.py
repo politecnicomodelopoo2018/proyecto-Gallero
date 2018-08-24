3374105fa80eea4567c53f909407aae05161c570
agusssss <agustingallero13@gmail.com>
2018-08-24 09:41:51 -0300

    proyecto musica

---
+++ Flask.py
@@ -0,0 +1,9 @@
+from flask import Flask, render_template
+app = Flask(__name__)
+
+@app.route("/")
+def iniciar():
+    return render_template("Prueba.html")
+
+if __name__ == "__main__":
+    app.run()
