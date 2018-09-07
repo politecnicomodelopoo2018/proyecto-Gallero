86540305b133329525d0731aae2830d3d230504b
agusssss <agustingallero13@gmail.com>
2018-09-07 10:16:13 -0300

    proyecto musica

--- Flask.py
+++ Flask.py
@@ -1,9 +1,11 @@
 from flask import Flask, render_template
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/login")
 def iniciar():
-    return render_template("Prueba.html")
-
+    return render_template("login.html")
+@app.route("/registro")
+def rergistro():
+    return render_template("registro.html")
 if __name__ == "__main__":
     app.run(debug=True)
