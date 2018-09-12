99ec69676fbf7eb65218c616dd4f068f320adef2
agusssss <agustingallero13@gmail.com>
2018-09-12 17:42:30 -0300

    proyecto musica

--- Flask.py
+++ Flask.py
@@ -1,11 +1,39 @@
-from flask import Flask, render_template
+from usuarios import Usuario
+from BD import DB
+from flask import Flask, render_template, request
 app = Flask(__name__)
+
+@app.route("/home")
+def home():
+    return render_template("home.html")
 
 @app.route("/login")
 def iniciar():
     return render_template("login.html")
-@app.route("/registro")
+
+@app.route("/registro",  methods=['GET', 'POST'])
 def rergistro():
+    if request.method == 'POST':
+        unUsuario = Usuario()
+        unUsuario.nombreUsuario = request.form.get("nombre")
+        unUsuario.apellidoUsuario = request.form.get("apellidos")
+        unUsuario.mail = request.form.get("mail")
+        unUsuario.contrasenia = request.form.get("contrasenia")
+
+        for item in Usuario().getUsuarios():
+            if item.mail == unUsuario.mail or item.nombreUsuario == unUsuario.nombreUsuario:
+                return render_template("registro.html")
+
+        unUsuario.registrarse()
+
+        return render_template("home.html")
+
+
     return render_template("registro.html")
+
+DB().setconnection('localhost','root','alumno','mydb')
+
+
 if __name__ == "__main__":
     app.run(debug=True)
+
--- usuarios.py
+++ usuarios.py
@@ -8,25 +8,25 @@ class Usuario(object):
     mail = None
 
     def registrarse(self):
-        DB().run("INSERT INTO usuario(idusuario,nombre,apellido,contraseña,mail)" +
+        DB().run("INSERT INTO usuario(idusuario,nombre,apellido,contrasenia,mail)" +
                  "VALUES (" + str(self.idUsuario) + ",'" + self.nombreUsuario + "','" + self.apellidoUsuario + "','" +
                  self.contrasenia + "','" + self.mail + "');")
 
     def cambiarContrasenia(self):
-        DB().run("UPDATE Usuario SET contraseña = '" + self.contrasenia + "' WHERE idusuario = " + str(self.idUsuario) + ";")
+        DB().run("UPDATE usuario SET contrasenia = '" + self.contrasenia + "' WHERE idusuario = " + str(self.idUsuario) + ";")
 
     @staticmethod
     def getUsuarios ():
         listaUsuarios = []
 
-        cursor = DB().run("SELECT * FROM Usuario")
+        cursor = DB().run("SELECT * FROM usuario")
 
         for item in cursor:
             unUsuario = Usuario()
             unUsuario.idUsuario = item['idusuario']
             unUsuario.nombreUsuario = item['nombre']
             unUsuario.apellidoUsuario = item['apellido']
-            unUsuario.contrasenia = item['contraseña']
+            unUsuario.contrasenia = item['contrasenia']
             unUsuario.mail = item['mail']
 
             listaUsuarios.append(unUsuario)
